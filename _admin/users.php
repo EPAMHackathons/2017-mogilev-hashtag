8f3eaf44b569347c5372630a851caa4cbc278488
Alexander Savchenkov <alex@mediapark.lt>
2017-04-08 11:01:38 +0300

    add tpl dirs

---
+++ _admin/users.php
@@ -0,0 +1,19 @@
+<?
+require_once('../include/init.inc.php');
+
+$act = get_postget_var('act');
+$id = get_postget_var('id');
+$ru = 'users.php';
+
+switch ($act) {
+
+}
+
+$items = new user_list();
+$items = $items->get();
+$tpl->assign('items', $items);
+
+$tpl->assign('menu_cat', 'user');
+$tpl->tpl = 'user';
+$tpl->render('admin/main.tpl');
+?>
--- migrations/admin_users_1.sql
+++ migrations/admin_users_1.sql
@@ -3,3 +3,6 @@ ALTER TABLE `admin_users` CHANGE `id` `id` INT(111) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `admin_users` ADD INDEX(`login`);
 ALTER TABLE `admin_users` ADD INDEX(`active`);
 UPDATE `telehelp`.`admin_users` SET `pwd` = '7bcb7bdedb320d2b4373db1bcbdec487' WHERE `admin_users`.`id` = 1;
+
+ALTER TABLE `user` ADD `active` TINYINT(1) NOT NULL AFTER `updated_at`;
+ALTER TABLE `user` CHANGE `active` `active` TINYINT(1) NOT NULL DEFAULT '0';
