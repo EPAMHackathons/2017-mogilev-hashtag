d2237d4d64473a09d403ec2005e319f047300483
IvanSidorenko <ivan.sidorenko@mediapark.com>
2017-04-08 10:56:44 +0300

    admin first part

--- _admin/user.php
+++ _admin/user.php
@@ -3,7 +3,7 @@ require_once('../include/init.inc.php');
 
 $act = get_postget_var('act');
 $id = get_postget_var('id');
-$ru = 'users.php';
+$ru = 'user.php';
 
 switch ($act) {
 	case 'save':
@@ -13,11 +13,9 @@ switch ($act) {
 	foreach ($_POST as $k => $v) if (!is_array($v)) $_POST[$k] = trim($v);
 	unset($_POST['id']);
 	
-		if (empty($_POST['active'])) $_POST['active'] = 0;
 	
 	
-	
-	$item = new users($id);
+	$item = new user($id);
 	$item->from_array($_POST);
 	
 	$item->save();
@@ -29,7 +27,7 @@ switch ($act) {
 case 'edit':
 
 	if ($id == -1) $id = NULL;
-	$item = new users($id);
+	$item = new user($id);
 	if (!empty($id) && empty($item->fields['id'])) throw_404();
 	$item = $item->to_array();
 	$tpl->assign('act', 'edit');
@@ -42,19 +40,10 @@ case 'delete':
 	validate_csrf_token();
 
 	if ($id == -1) $id = NULL;
-	$item = new users($id);
+	$item = new user($id);
 	$item->delete();
 
 	flashbag_put('Изменения сохранены');
-	redirect($ru);
-	break;
-
-	case 'activate':
-	db_query("UPDATE users SET active='1' WHERE id='$id'");
-	redirect($ru);
-	break;
-	case 'deactivate':
-	db_query("UPDATE users SET active='0' WHERE id='$id'");
 	redirect($ru);
 	break;
 
@@ -62,11 +51,11 @@ case 'delete':
 
 }
 
-$items = new users_list();
+$items = new user_list();
 $items = $items->get();
 $tpl->assign('items', $items);
 
-$tpl->assign('menu_cat', 'users');
-$tpl->tpl = 'users';
+$tpl->assign('menu_cat', 'user');
+$tpl->tpl = 'user';
 $tpl->render('admin/main.tpl');
 ?>
--- include/models/user.cls.php
+++ include/models/user.cls.php
@@ -1,22 +1,24 @@
 <?
-	class users extends db_row {
+	class user extends db_row {
 		
 		function __construct($id = NULL) {
 			//$this->use_db = DB_USERS_NAME;
-			$this->table = 'users';
+			$this->table = 'user';
 			$this->pri_key = 'id';
 			$this->autoinc = 1;
 			//$this->parent_key = '';
-			//$this->url_prefix = '/users/';
+			//$this->url_prefix = '/user/';
 			//$this->url_postfix = '.html';
 
 
 			$this->fields['id'] = NULL;
+			$this->fields['first_name'] = NULL;
+			$this->fields['last_name'] = NULL;
 			$this->fields['username'] = NULL;
-			$this->fields['telegram_id'] = NULL;
-			$this->fields['active'] = NULL;
-
-            parent::__construct($id);
+			$this->fields['created_at'] = NULL;
+			$this->fields['updated_at'] = NULL;
+			
+			parent::__construct($id);
 		}
 
 		function get_more_data() {
@@ -29,11 +31,11 @@
 
 	}
 
-	class users_list extends db_list {
+	class user_list extends db_list {
 
 		function __construct() {
-			$this->single_element_class = 'users';
-			$this->table = 'users';
+			$this->single_element_class = 'user';
+			$this->table = 'user';
 			$this->sort_by = 'id';
 			$this->sort_mode = 'desc';
 			$this->items_per_page = 10;
